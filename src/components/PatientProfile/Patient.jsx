4238aeccbcf253c128b15d5882cdf163cc5d4973
Anushiniananth <anushiniananth@gmail.com>
2025-07-17 23:41:41 +0530

    added

--- src/components/PatientProfile/Patient.jsx
+++ src/components/PatientProfile/Patient.jsx
@@ -3,15 +3,14 @@ import { Routes, Route } from "react-router-dom";
 
 import PatientProfile from "./PatientProfile";
 import EditProfileModel from "./EditProfileModel";
+import PatientHistory from "./PatientHistory";
 
 const Patient = () => {
   return (
     <Routes>
-      {/* Correct index route for /patient/ */}
       <Route index element={<PatientProfile />} />
-
-      {/* Route for /patient/edit-profile */}
       <Route path="edit-profile" element={<EditProfileModel />} />
+      <Route path="history" element={<PatientHistory />} /> ✅ Route fixed
     </Routes>
   );
 };

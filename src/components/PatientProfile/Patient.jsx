15b5751eae7a0f64e8e4c89d976d7c4d6b490445
innocentkishore <smartkishorekumar57@gmail.com>
2025-07-22 14:10:26 +0530

    fix-error

--- src/components/PatientProfile/Patient.jsx
+++ src/components/PatientProfile/Patient.jsx
@@ -3,15 +3,14 @@ import { Routes, Route } from "react-router-dom";
 
 import PatientProfile from "./PatientProfile";
 import EditProfileModel from "./EditProfileModel";
-import { PatientHistory } from "./PatientHistory";
-//import PatientHistory from "./PatientHistory";
+import PatientHistory from "./PatientHistory"; // ✅ default import
 
 const Patient = () => {
   return (
     <Routes>
       <Route index element={<PatientProfile />} />
       <Route path="edit-profile" element={<EditProfileModel />} />
-      <Route path="history" element={<PatientHistory />} /> ✅ Route fixed
+      <Route path="history" element={<PatientHistory />} />
     </Routes>
   );
 };

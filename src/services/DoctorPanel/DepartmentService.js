9e3b74187770a1f69c154510648187bb0d11b8de
Munirathinam_E <munirathinam6724@gmail.com>
2025-08-01 14:15:08 +0530

    Updated

--- src/services/DoctorPanel/DepartmentService.js
+++ src/services/DoctorPanel/DepartmentService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE = "http://localhost:8082/api/departments";
+const API_BASE = "https://doctorpanel-backend.onrender.com/api/departments";
 
 export const fetchDepartments = async () => {
   const res = await axios.get(API_BASE);

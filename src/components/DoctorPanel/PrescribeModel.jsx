9cc5dcf3d278a9dd2844cfc61771673de02cf081
Munirathinam_E <munirathinam6724@gmail.com>
2025-08-06 00:04:02 +0530

    added some

---
+++ src/components/DoctorPanel/PrescribeModel.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Dialog, DialogContent } from "./ui/dialog";
+import PrescriptionForm from "../../Pages/DoctorPanel/PrescriptionForm";
+
+const PrescribeModal = ({
+  isOpen,
+  onClose,
+  appointmentId,
+  doctorId,
+  onSuccess,
+}) => {
+  // Debug logs to verify correct IDs are being passed
+  console.log("PrescribeModal props:", {
+    appointmentId,
+    doctorId,
+    isOpen
+  });
+
+  return (
+    <Dialog open={isOpen} onOpenChange={onClose}>
+      <DialogContent className="max-w-5xl w-full max-h-[90vh] p-0 overflow-hidden rounded-2xl">
+        <div className="h-[90vh] flex flex-col">
+          <div className="flex-1 overflow-y-auto px-6 py-4">
+            <PrescriptionForm
+              appointmentId={appointmentId}
+              doctorId={doctorId}
+              onClose={onClose}
+              onSuccess={onSuccess}
+            />
+          </div>
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default PrescribeModal;
--- src/services/DoctorPanel/GetDoctorsBySpecialty.js
+++ src/services/DoctorPanel/GetDoctorsBySpecialty.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 const BASE_URL = 'https://doctorpanel-backend.onrender.com/api/doctor';
 
-export const getDoctorsBySpecialty = (specialty) => {
-  return axios
-    .get(`${BASE_URL}/by-specialty`, { params: { specialty } })
-    .then((res) => res.data);
+export const getDoctorsBySpecialty = async (specialty) => {
+  const res = await axios
+    .get(`${BASE_URL}/by-specialty`, { params: { specialty } });
+  return res.data;
 };
